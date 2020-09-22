b744f7957832cce75ca97e33871c56e5bb718b3a
Tejeswara Sai Appikatla <appikatlahome@gmail.com>
2020-09-22 23:50:40 +0530

    Drivers License Assignment Python Basics Module Submitted Now on Udemy

---
+++ Python Basics/DriversLicenseAssignment.py
@@ -0,0 +1,10 @@
+firstname = "Tejeswara Sai"
+lastname = "Appikatla"
+age = 18
+ssn = 180030507
+height = "5.8"
+weight = 75
+print("Driver Details")
+print("Name of the Driver is "+firstname+" "+lastname+" His Age is "+str(age)+" With SSN "+str(ssn)+" of height "+height+" With Weight "+str(weight))
+
+print("")
